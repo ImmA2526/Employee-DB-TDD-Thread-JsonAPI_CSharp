a8dd3889120855f02fea884dd806b8c8c67f6cf5
Imran2526 <imraninfo97@gmail.com>
2021-01-03 10:45:20 +0530

    [Imran] Add: Retrive All Record Form Database

--- EmployeeAdo_TDD/EmployeeRepo.cs
+++ EmployeeAdo_TDD/EmployeeRepo.cs
@@ -29,5 +29,55 @@ namespace EmployeePayrol_DB
                 return false;
             }
         }
+        /// <summary>
+        ///UC 2 Gets all employee.
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        public int GetAllEmployee()
+        {
+            try
+            {
+                int count = 0;
+                EmployeeModel Model = new EmployeeModel();
+                using (this.Connection)
+                {
+                    string query = @"Select * from EmployeePayroll;";
+                    SqlCommand CMD = new SqlCommand(query, this.Connection);
+                    this.Connection.Open();
+                    SqlDataReader reader = CMD.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Model.Id = reader.GetInt32(0);
+                            Model.name = reader.GetString(1);
+                            Model.basic_pay = reader.GetDecimal(2);
+                            Model.start_Date = reader.GetDateTime(3);
+                            Model.gender = Convert.ToChar(reader.GetString(4));
+                            Model.phoneNumber = reader.GetString(5);
+                            Model.department = reader.GetString(6);
+                            Model.address = reader.GetString(7);
+                            Model.deduction = reader.GetDouble(8);
+                            Model.taxable = reader.GetSqlSingle(9);
+                            Model.netpay = reader.GetSqlSingle(10);
+                            Model.income_tax = reader.GetDouble(11);
+                            Console.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}", Model.Id, Model.name, Model.basic_pay, Model.start_Date, Model.gender, Model.phoneNumber, Model.department, Model.address, Model.deduction, Model.taxable, Model.netpay, Model.income_tax);
+                            count++;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Found");
+                    }
+                    reader.Close();
+                    this.Connection.Close();
+                }
+                return count;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
--- EmployeePayrollsTester/UnitTest1.cs
+++ EmployeePayrollsTester/UnitTest1.cs
@@ -2,6 +2,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EmployeePayrol_DB;
 namespace EmployeePayrollsTester
 {
+    /// <summary>
+    /// Uc 1 Check Connection
+    /// </summary>
+    
     [TestClass]
     public class UnitTest1
     {
@@ -13,5 +17,19 @@ namespace EmployeePayrollsTester
             bool result = true;
             Assert.AreEqual(result, Check);
         }
+
+        /// <summary>
+        /// Uc 2 Retrive Record
+        /// </summary>
+        [TestMethod]
+        public void RetriveData_whenQueryGiven()
+        {
+            EmployeeRepo retrive = new EmployeeRepo();
+            int retrives = retrive.GetAllEmployee();
+            int result = 12;
+            Assert.AreEqual(retrives, result);
+        }
     }
+
+
 }
