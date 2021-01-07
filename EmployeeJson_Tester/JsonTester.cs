c6caaa94b77fd676925ed55d28df2e6bb1e68aae
Imran2526 <imraninfo97@gmail.com>
2021-01-07 17:26:19 +0530

    [Imran] Add: Ability to Add Employee By API

--- EmployeeJson_Tester/JsonTester.cs
+++ EmployeeJson_Tester/JsonTester.cs
@@ -8,7 +8,6 @@ using Newtonsoft.Json;
 
 namespace EmployeePayroll_Json_Tester
 {
-
     public class EmployeeModels
     {
         public int Id { get; set; }
@@ -20,7 +19,6 @@ namespace EmployeePayroll_Json_Tester
     public class JSONTester
     {
         RestClient client;
-
         public object JsonConvertor { get; private set; }
 
         [TestInitialize]
@@ -28,20 +26,17 @@ namespace EmployeePayroll_Json_Tester
         {
             client = new RestClient("http://localhost:4000");
         }
-
         private IRestResponse GetEmployeeList()
         {
             RestRequest request = new RestRequest("/Employee", Method.GET);
             IRestResponse response = client.Execute(request);
             return response;
         }
-
         /// <summary>
         /// UC 1 Retrive Record 
         /// </summary>
         
         [TestMethod]
-
         public void OnCalling_GetApi_ReturnList()
         {
             IRestResponse response = GetEmployeeList();
@@ -53,6 +48,25 @@ namespace EmployeePayroll_Json_Tester
             {
                 System.Console.WriteLine("ID: ", e.Id, "Name:", e.Name, "Basic Salary: ", e.Salary);
             }
+        }
+
+        /// <summary>
+        /// UC 2 Shoulds the return add employee.
+        /// </summary>
+
+        [TestMethod]
+        public void ShouldReturnAddEmployee_BY_API()
+        {
+            RestRequest request = new RestRequest("/Employee", Method.POST);
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("Name", "Asif");
+            jObjectbody.Add("Salary", "90000");
+            request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            EmployeeModels dataResponse = JsonConvert.DeserializeObject<EmployeeModels>(response.Content);
+            Assert.AreEqual("Asif", dataResponse.Name);
+            Assert.AreEqual("90000", dataResponse.Salary);
         }
     }
 }
